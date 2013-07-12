d28495de4f216176886811878094bd6fff2d7808
Adan Rodriguez <mario2485@yahoo.com>
2013-07-12 06:58:27 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
  
-var buf = new Buffer(readFileSync('index.html'));
+var buf = new Buffer(fs.readFileSync('index.html'));
  
 app.get('/', function(request, response) {
   response.send(buf.toString());
